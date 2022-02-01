1a3dbea56a0337e8735ad487f112512a2eb91cd2
Filipko <98056125+FilipKopecny@users.noreply.github.com>
2022-02-01 20:14:42 +0100

    Počiatočné súbory pre projekt MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    if (uhol > 0) {
+        uhol += -10
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    uhol = 90
+})
+input.onButtonPressed(Button.B, function () {
+    if (uhol < 180) {
+        uhol += 10
+    }
+})
+let uhol = 0
+uhol = 90
+basic.forever(function () {
+    pins.servoWritePin(AnalogPin.P16, uhol)
+})
